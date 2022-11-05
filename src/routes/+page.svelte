9dbd8c10a722c563a60e29bea5056e7d597010de
Christopher Chang <chiggiechang@gmail.com>
2022-11-04 21:14:12 -0700

    Style accounts table with Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	// import '../app.css';
+	import '../app.css';
 	import ky from 'ky';
 
 	export let data: PageData;
@@ -22,31 +22,49 @@
 	};
 </script>
 
-<h1>Accounts</h1>
+<div class="m-4 mx-auto w-max">
+	<h1 class="text-2xl mb-3">Accounts</h1>
 
-<table>
-	<thead>
-		<tr>
-			<td>ID</td>
-			<td>Name</td>
-			<!-- Invisible button column: -->
-			<td />
-		</tr>
-	</thead>
-	<tbody>
-		{#each data.accounts as account}
+	<table class="table-auto bg-white">
+		<thead>
 			<tr>
-				<td>{account.id}</td>
-				<td>{account.name}</td>
+				<td class="border-gray-500 border-y-4 divide-x-gray-300 font-bold px-6 py-3 bg-blue-100">ID</td>
+				<td class="border-gray-500 border-y-4 divide-x-gray-300 font-bold px-6 py-3 bg-blue-100">Name</td>
+				<!-- Invisible button column: -->
+				<td />
 			</tr>
-		{/each}
-		<tr>
-			<td>TBD</td>
-			<td><input type="text" bind:value={newAccount.name} /></td>
-			<td><button on:click={onCreateClick}>Create</button></td>
-		</tr>
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			{#each data.accounts as account}
+				<tr>
+					<td class="border border-y-4 px-6 py-3">{account.id}</td>
+					<td class="border border-y-4 px-6 py-3">{account.name}</td>
+					<td class="px-2">
+						<button class="bg-red-500 text-white text-sm shadow-md rounded-lg p-3 px-6 pt-2 w-full hover:bg-red-400">
+							Delete
+						</button>
+					</td>
+				</tr>
+			{/each}
+			<tr>
+				<td class="border border-y-4 px-6 py-3">TBD</td>
+				<td class="border border-y-4 px-6 py-3"
+					><input
+						type="text"
+						bind:value={newAccount.name}
+						class="bg-blue-100 px-3 rounded-lg shadow-md focus:outline-none"
+					/></td
+				>
+				<td class="p-2"
+					><button
+						class="bg-blue-500 text-white text-lg shadow-md rounded-lg p-3 px-6 pt-2 w-full hover:bg-blue-400"
+						on:click={onCreateClick}>Create</button
+					></td
+				>
+			</tr>
+		</tbody>
+	</table>
+</div>
 
 <style>
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [
+					'ui-sans-serif',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji'
+				]
+			}
+		}
+	},
+	plugins: []
+};
