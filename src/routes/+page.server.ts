0c448f22fe39e0ea540f0b5d708ae9bf76767c15
Christopher Chang <chiggiechang@gmail.com>
2022-11-12 15:14:23 -0800

    Use transaction when creating child account

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -29,27 +29,27 @@ export const actions: Actions = {
 		const parentAccountId = data.get('parent_account_id');
 		const newAccountName = data.get('new_account_name');
 
-		// FIXME(Chris): Use some kind of transaction
-
 		const db = getDB();
 
-		const { lastInsertRowid } = db
-			.prepare('INSERT INTO account(name) VALUES (?);')
-			.run(newAccountName);
+		db.transaction(() => {
+			const { lastInsertRowid } = db
+				.prepare('INSERT INTO account(name) VALUES (?);')
+				.run(newAccountName);
 
-		// NOTE(Chris): By using a cross join of the parent and the child, we will
-		// place all of our desired ancestor accounts' IDs into p.parent_id. We
-		// will also place all of the IDs of the descendants of our new account
-		// (including the account's own ID) into c.child_id.
-		// In this specific case, our freshly-made account will have no descendants
-		// (besides itself), so its own ID will be its only descendant.
-		db.prepare(
-			`INSERT INTO account_parent_child(parent_id, child_id, depth)
+			// NOTE(Chris): By using a cross join of the parent and the child, we will
+			// place all of our desired ancestor accounts' IDs into p.parent_id. We
+			// will also place all of the IDs of the descendants of our new account
+			// (including the account's own ID) into c.child_id.
+			// In this specific case, our freshly-made account will have no descendants
+			// (besides itself), so its own ID will be its only descendant.
+			db.prepare(
+				`INSERT INTO account_parent_child(parent_id, child_id, depth)
              SELECT p.parent_id, c.child_id, p.depth + c.depth + 1
              FROM account_parent_child p
                  CROSS JOIN account_parent_child c
              WHERE p.child_id = ? AND c.parent_id = ?`
-		).run(parentAccountId, lastInsertRowid);
+			).run(parentAccountId, lastInsertRowid);
+		})();
 	},
 	delete_with_all_children: async (event) => {
 		const data = await event.request.formData();
