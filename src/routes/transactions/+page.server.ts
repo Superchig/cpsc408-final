af5fa5e5af7a4a355e376f6076378e84c567378d
Christopher Chang <chiggiechang@gmail.com>
2022-12-06 20:47:59 -0800

    Implement back-end for transaction searching

--- src/routes/transactions/+page.server.ts
+++ src/routes/transactions/+page.server.ts
@@ -3,34 +3,70 @@ import { getDB } from '$lib/server/db';
 import { findAllAccounts } from '$lib/server/accounts';
 import type { DebitCredit, Transaction } from '$lib/transaction';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async (event) => {
   const db = getDB();
 
   const accounts = findAllAccounts(db);
 
-  // Obtain all transactions
+  // Obtain relevant transactions
+
+  const searchParams = {
+    date: event.url.searchParams.get('date'),
+    accountId: Number(event.url.searchParams.get('account_id')),
+    description: event.url.searchParams.get('description')
+  };
+
+  let bindParams: any = {};
+  let sqlConditions = [];
+
+  if (searchParams.date) {
+    bindParams.date = searchParams.date;
+    sqlConditions.push('financial_transaction.date = $date');
+  }
+
+  if (searchParams.accountId) {
+    bindParams.account_id = searchParams.accountId;
+    // FIXME(Chris): Use table view for triple-join here
+    sqlConditions.push(`financial_transaction.id IN
+                            (SELECT DISTINCT transaction_id
+                             FROM financial_transaction
+                                      INNER JOIN debit_credit on financial_transaction.id = debit_credit.transaction_id
+                                      INNER JOIN account on debit_credit.account_id = account.id
+                             WHERE account_id = $account_id -- ID
+                          ORDER BY financial_transaction.date)`);
+  }
+
+  if (searchParams.description) {
+    bindParams.description = searchParams.description;
+    sqlConditions.push("financial_transaction.description LIKE '%' || $description || '%'");
+  }
+
+  let sqlBeforeSubstitution = `SELECT financial_transaction.id AS transaction_id,
+                                      financial_transaction.description AS transaction_description,
+                                      financial_transaction.date AS transaction_date,
+                                      debit_credit.id AS debit_credit_id,
+                                      debit_credit.amount AS debit_credit_amount,
+                                      debit_credit.account_id
+                               FROM financial_transaction
+                                   INNER JOIN debit_credit on financial_transaction.id = debit_credit.transaction_id
+                                   INNER JOIN account on debit_credit.account_id = account.id`;
+
+  if (sqlConditions.length > 0) {
+    sqlBeforeSubstitution += '\n';
+    
+    sqlBeforeSubstitution += 'WHERE ' + sqlConditions.join('\n  AND ');
+  }
+
+  sqlBeforeSubstitution += '\nORDER BY financial_transaction.date';
 
   const transactionRows: {
     transaction_id: number;
     transaction_date: string;
-    transaction_description: string,
+    transaction_description: string;
     debit_credit_id: number;
     debit_credit_amount: number;
     account_id: number;
-  }[] = db
-    .prepare(
-      `SELECT financial_transaction.id AS transaction_id,
-              financial_transaction.description AS transaction_description,
-              financial_transaction.date AS transaction_date,
-              debit_credit.id AS debit_credit_id,
-              debit_credit.amount AS debit_credit_amount,
-              debit_credit.account_id
-       FROM financial_transaction
-           INNER JOIN debit_credit on financial_transaction.id = debit_credit.transaction_id
-           INNER JOIN account on debit_credit.account_id = account.id
-       ORDER BY financial_transaction.date;`
-    )
-    .all();
+  }[] = db.prepare(sqlBeforeSubstitution).all(bindParams);
 
   // Convert transactionRows into Transaction[]
 
